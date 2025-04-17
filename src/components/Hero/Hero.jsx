b7bdc3c001cee02e4185fd9a9ba2dcca8f9b1b16
Guru4118 <165055820+Guru4118@users.noreply.github.com>
2025-04-17 12:03:28 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,17 +9,24 @@ import heroImage from "./hero.jpg";
 export const Hero = () => {
   return (
     <section className={styles.container}>
-      <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Neha</h1>
-        <p className={styles.description}>
-        Enthusiastic and detail-oriented Full Stack Developer with experience in building dynamic, scalable, and user-friendly web applications. Passionate about creating seamless digital experiences and optimizing performance for better usability.        </p>
-        <a href="mailto:grandheneha@email.com" className={styles.contactBtn}>
-          Contact Me
-        </a>
-      </div>
-   
+  <div className={styles.content}>
+    <h1 className={styles.title}>Hi, I'm Neha</h1>
+    <p className={styles.description}>
+      Enthusiastic and detail-oriented Full Stack Developer with experience in building dynamic, scalable, and user-friendly web applications. Passionate about creating seamless digital experiences and optimizing performance for better usability.
+    </p>
 
-<img src={heroImage} className="rounded-full" alt="Hero image of me" className={styles.heroImg} />;
+    {/* Buttons Wrapper */}
+    <div style={{ display: 'flex', gap: '1rem' }}>
+      <a href="mailto:grandheneha@email.com" className={styles.contactBtn}>
+        Contact Me
+      </a>
+      <a target="new" href="https://drive.google.com/file/d/1BXKN3Ed1KUDLHF9Uryun8p3TdKv88ifl/view?usp=sharing" className={styles.contactBtn}>
+        CV
+      </a>
+    </div>
+    </div>
+
+<img src={heroImage}alt="Hero image of me" className={ styles.heroImg} />;
 
       
 
